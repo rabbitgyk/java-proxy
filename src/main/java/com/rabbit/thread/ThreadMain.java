e0181655a4607bd43386f09604454cf5168cc0c4
guoyankui <yankuiguo@gmail.com>
2017-12-03 14:07:35 +0800

    a thread test

---
+++ src/main/java/com/rabbit/thread/ThreadMain.java
@@ -0,0 +1,13 @@
+package com.rabbit.thread;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @Author: guoyankui
+ * @DATE: 27/11/2017 11:53 PM
+ */
+public class ThreadMain {
+    public static void main(String[] args) {
+
+    }
+}
